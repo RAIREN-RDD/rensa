7d13e4f0ec2aed029cfa044364751ab01436cfdd
kai5041._ <rdd.contact@rairen.net>
2026-05-28 21:20:49 +0200

    予備

--- src/commands/build.cpp
+++ src/commands/build.cpp
@@ -1,176 +1,229 @@
 #include "rensa/commands.hpp"
 #include "rensa/core.hpp"
 #include "rensa/log.hpp"
+#include "rensa/types.hpp"
 
 #include <filesystem>
 #include <fstream>
+#include <functional>
+#include <iterator>
 #include <print>
+#include <set>
 #include <sstream>
 #include <string>
 #include <unordered_set>
 
+#include <yaml-cpp/yaml.h>
+
 namespace rairen::rensa {
 
-static Path find_build_file(Path start) {
-  start = fs::absolute(start);
+using Vars = UnorderedMap<String, std::set<String>>;
 
-  while (true) {
-    Path candidate = start / "build.rensa";
+#define try_assign(var, varname)                                               \
+  if (!vars.contains(varname)) {                                               \
+    rensa::log(LogLevel::Error, "Argument '{}' missing", varname);             \
+    return SystemStatus::Error;                                                \
+  }                                                                            \
+  StringView var = vars[varname]
 
-    if (fs::exists(candidate) && fs::is_regular_file(candidate)) {
-      return candidate;
+#define try_assign_opt(var, name)                                              \
+  StringView var = (vars).contains(name) ? (vars)[name] : StringView {}
+
+struct Builder {
+  Vars vars;
+
+  void init(Vars &vars) { this->vars = vars; }
+
+  virtual ~Builder() = default;
+
+  virtual SystemStatus build() = 0;
+};
+
+#undef try_assign
+
+struct Target {
+  Vars vars;
+  Builder *builder = nullptr;
+};
+
+using File = OrderedMap<u64, Vector<String>>;
+using Iter = File::iterator;
+
+Vars create_vars(const File &file, Iter &it) {
+  Vars vars;
+
+  for (++it; it != file.end(); ++it) {
+    auto &[line_number, tokens] = *it;
+
+    if (tokens.empty()) {
+      continue;
     }
 
-    if (!start.has_parent_path() || start == start.root_path()) {
-      break;
+    if (tokens[0] == "END") {
+      return vars;
     }
 
-    start = start.parent_path();
+    if (tokens.size() < 2) {
+      rensa::log(LogLevel::Error, "Invalid variable declaration on line '{}'",
+                 line_number);
+
+      continue;
+    }
+
+    const String &key = tokens[0];
+
+    String value;
+
+    if (tokens.size() == 1) {
+      vars[key] = {};
+      continue;
+    }
+
+    if (tokens.size() == 2) {
+      rensa::log(LogLevel::Error, "Missing value on line '{}'", line_number);
+
+      continue;
+    }
+
+    String op = tokens[1];
+
+    if (!std::set<String>({"=", "?=", "+="}).contains(op)) {
+      rensa::log(LogLevel::Error, "Unknown operator '{}' on line {}", op,
+                 line_number);
+
+      continue;
+    }
+
+    if (op == "=" || op == "?=") {
+      vars[key] = std::set<String>(tokens.begin() + 2, tokens.end());
+      continue;
+    }
+
+    if (op == "+=") {
+      vars[key].insert(tokens.begin() + 2, tokens.end());
+      continue;
+    }
   }
 
-  return {};
+  rensa::log(LogLevel::Error, "Unterminated VARS block");
+
+  return vars;
 }
 
-enum class TokenType {
-  Identifier,
-  Keyword,
-  StringLiteral,
+Target create_target(const File &file, Iter &it) {
+  Target target;
 
-  Operator,
-  BlockStart, // TARGET, VARS, DEPENDENCIES
-  BlockEnd,   // END
+  for (++it; it != file.end(); ++it) {
+    auto &[line_number, tokens] = *it;
 
-  VariableRef, // ${PROJECT_NAME}
-
-  EndOfFile,
-  NewLine
-};
-class Token {
-public:
-  TokenType type;
-  std::string value;
-  u64 line_number;
-
-  Token(TokenType _type, std::string _value, u64 _line_number)
-      : type(_type), value(std::move(_value)), line_number(_line_number) {}
-};
-
-RENSA_COMMAND(build) {
-  Path start_dir = ".";
-
-  if (!args.empty()) {
-    start_dir = args[0];
-  }
-
-  if (!fs::exists(start_dir)) {
-    log(LogLevel::Error, "Path '{}' does not exist", start_dir.string());
-    return SystemStatus::Error;
-  }
-
-  if (!fs::is_directory(start_dir)) {
-    log(LogLevel::Error, "Path '{}' is not a directory", start_dir.string());
-    return SystemStatus::Error;
-  }
-
-  Path build_file = find_build_file(start_dir);
-
-  if (build_file.empty()) {
-    log(LogLevel::Error, "No build.rensa found starting from '{}'",
-        start_dir.string());
-    return SystemStatus::Error;
-  }
-
-  struct Statuses {
-    bool inside_target = false;
-    bool inside_var = false;
-    bool inside_depends = false;
-  };
-
-  build_file = fs::weakly_canonical(build_file);
-
-  std::ifstream f(build_file);
-
-  String line;
-
-  Statuses statuses;
-  usize line_number = 0;
-
-  UnorderedSet<String> keywords = {"PROJECT_NAME", "PROJECT_VERSION"};
-  UnorderedSet<String> block_starts = {"TARGET", "VARS", "DEPENDENCIES"};
-
-  UnorderedSet<String> operators = {"=", "+=", "?="};
-
-  Vector<Token> tokens;
-  String token;
-
-  bool inside_var = false;
-
-  while (std::getline(f, line)) {
-    line_number++;
-
-    if (line.empty())
+    if (tokens.empty()) {
       continue;
+    }
 
-    std::stringstream iss(line);
-    while (iss >> token) {
-      if (token == "//")
-        break;
+    if (tokens[0] == "VARS") {
+      target.vars = create_vars(file, it);
+      continue;
+    }
 
-      bool is_keyword = keywords.contains(token);
-      if (is_keyword) {
-        tokens.push_back(Token(TokenType::Keyword, token, line_number));
-        continue;
-      }
-
-      bool is_block = block_starts.contains(token);
-      if (is_block) {
-        tokens.push_back(Token(TokenType::BlockStart, token, line_number));
-        if (token == "VARS") {
-          inside_var = true;
-        }
-        continue;
-      }
-
-      bool is_operator = operators.contains(token);
-      if (is_operator) {
-        tokens.push_back(Token(TokenType::Operator, token, line_number));
-        continue;
-      }
-
-      if (inside_var) {
-        tokens.push_back(Token(TokenType::Identifier, token, line_number));
-        continue;
-      }
-
-      if (token == "END") {
-        tokens.push_back(Token(TokenType::BlockEnd, "", line_number));
-        inside_var = false;
-        continue;
-      }
-
-      StringView token_view(token);
-      if (token_view.starts_with("RS{")) {
-        if (!token_view.ends_with("}")) {
-          rensa::log(LogLevel::Error,
-                     "RS expression at line {} not closed - '}}' missing",
-                     line_number);
-          return SystemStatus::Error;
-        }
-
-        tokens.push_back(Token(
-            TokenType::VariableRef,
-            String(token_view.substr(3, token_view.size() - 4)), line_number));
-
-        continue;
-      }
-
-      tokens.push_back(Token(TokenType::StringLiteral, token, line_number));
+    if (tokens[0] == "END") {
+      return target;
     }
   }
 
-  for (auto token : tokens) {
-    std::println("{} = {}", int(token.type), token.value);
+  rensa::log(LogLevel::Error, "Unterminated TARGET block");
+
+  return target;
+}
+
+RENSA_COMMAND(build) {
+  UnorderedMap<String, Target> targets;
+
+  String target_name = args.size() > 1 ? args[1] : "";
+
+  std::ifstream file_in("build.rensa");
+
+  if (!file_in.is_open()) {
+    rensa::log(LogLevel::Error, "Failed to open 'build.rensa'");
+    return SystemStatus::Error;
   }
+
+  File file;
+
+  String line;
+  String token;
+  u64 line_number = 0;
+
+  while (std::getline(file_in, line)) {
+    ++line_number;
+
+    if (line.empty()) {
+      continue;
+    }
+
+    std::stringstream ss(line);
+
+    Vector<String> tokens;
+
+    while (ss >> token) {
+      if (token == "#") {
+        break;
+      }
+
+      tokens.push_back(token);
+    }
+
+    if (!tokens.empty()) {
+      file[line_number] = std::move(tokens);
+    }
+  }
+
+  for (auto it = file.begin(); it != file.end(); ++it) {
+    const auto &[line_index, tokens] = *it;
+
+    if (tokens.empty()) {
+      continue;
+    }
+
+    if (tokens[0] == "TARGET") {
+
+      if (tokens.size() < 2) {
+        rensa::log(LogLevel::Error, "Target name missing on line '{}'",
+                   line_index);
+
+        return SystemStatus::Error;
+      }
+
+      if (tokens.size() > 2) {
+        rensa::log(LogLevel::Error, "Expected only target name on line '{}'",
+                   line_index);
+
+        return SystemStatus::Error;
+      }
+
+      const String &name = tokens[1];
+
+      if (targets.contains(name)) {
+        rensa::log(LogLevel::Error, "Duplicate target '{}' on line '{}'", name,
+                   line_index);
+
+        return SystemStatus::Error;
+      }
+
+      targets[name] = create_target(file, it);
+      std::println("Target {} created", name);
+
+      continue;
+    }
+
+    if (tokens[0] == "END") {
+      rensa::log(LogLevel::Error, "Unexpected END directive on line '{}'",
+                 line_index);
+
+      return SystemStatus::Error;
+    }
+  }
+
+  std::println("{}", targets["rensa"].vars);
 
   return SystemStatus::Success;
 }
