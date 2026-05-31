7c75f6bd6a1e6dfda4e45317323baaecc09f500e
kai5041 <rdd.contact@rairen.net>
2026-06-01 01:07:42 +0200

    始

--- include/rensa/version.hpp
+++ include/rensa/version.hpp
@@ -3,9 +3,9 @@
 
 namespace rairen::rensa {
 
-constexpr int RENSA_VERSION_MAJOR = 1;
+constexpr int RENSA_VERSION_MAJOR = 0;
 constexpr int RENSA_VERSION_MINOR = 0;
-constexpr int RENSA_VERSION_PATCH = 0;
+constexpr int RENSA_VERSION_PATCH = "0-alpha-0";
 
 #ifndef RENSA_COMMIT_HASH
 constexpr const char *RENSA_COMMIT_HASH = "defined by VCS";
